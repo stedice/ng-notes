bec5d170ec0e6f80f6596124ffc1bf32eb1904e3
stedice <stefanodc@mela2.local>
2017-09-07 11:20:47 +0200

    new note service

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Note } from '../../classes/note';
+import { NoteService } from '../../services/note.service';
 import { Category } from '../../classes/category';
 import { CategoryService } from '../../services/category.service';
 
@@ -7,46 +8,44 @@ import { CategoryService } from '../../services/category.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [CategoryService]
+  providers: [NoteService, CategoryService]
 })
 
 export class AppComponent {
   title = 'Angular4 Notes';
-  currentId: number;
-
-  notes: Note[] = [
-    { id: 1, title: '1st note', content: 'this is the first note', category: 1, created: '2017-09-03T15:29:24.034Z'},
-    { id: 2, title: '2nd note', content: 'this is the second note', category: 2, created: '2017-09-04T09:10:24.034Z'},
-    { id: 3, title: '3rd note with a very long title', content: 'this is the third note', category: 3, created: '2017-09-04T12:32:24.034Z'},
-  ];
+  notes: Note[];
   activeNote: Note;
+  categories: Category[];
 
-  constructor(private categoryService: CategoryService){
-    this.currentId = this.notes.map( el => el.id )
-                               .reduce( (max, cur) => Math.max( max, cur ), 0 ) + 1;
+  constructor(private noteService: NoteService, private categoryService: CategoryService){
+    this.notes = this.getNotes();                          
+    this.categories = this.getCategories();
+  }
+
+  getNotes = () => {
+    return this.noteService.getNotes();
   }
 
   selectNote = (note) => {
     this.activeNote = note;
   }
 
-  getCategory = (note) => {
-    return this.categoryService.getCategory(note.category);
-  }
-
   addNote = () => {
-    const date = new Date().toJSON();
-    const newNote: Note = {id: this.currentId++, title: 'untitled', content: '...', category: 1, created: date};
-    this.notes.push(newNote);
-    this.activeNote = newNote;
+    this.activeNote = this.noteService.addNote();;
   }
 
   removeNote = (note:Note = this.activeNote) => {
-    const i = this.notes.indexOf(note);
-    if (i !== -1) {
-        this.notes.splice(i, 1);
-    }
+    this.notes = this.noteService.removeNote(note);;
     this.activeNote = undefined;
   }
+
+  getCategories = () => {
+    return this.categoryService.getCategories();
+  }
+
+  getCategory = (note) => {
+    return this.categories.find(x => x.id === note.category);
+  }
+
 
 }
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Note } from '../classes/note';
 
 @Injectable()
 export class NoteService {
+	list: Note[] = [
+    { 
+    	id: 1, 
+      title: '1st note', 
+      content: 'this is the first note', 
+      category: 1, 
+      created: '2017-09-03T15:29:24.034Z'
+    }, { 
+      id: 2, 
+      title: '2nd note', 
+      content: 'this is the second note', 
+      category: 2, 
+      created: '2017-09-04T09:10:24.034Z'
+    }, { 
+      id: 3, 
+      title: '3rd note with a very long title (it is very long indeed)', 
+      content: 'this is the third note', 
+      category: 3, 
+      created: '2017-09-04T12:32:24.034Z'
+    },
+  ];
+  currentId: number;
 
-  constructor() { }
+  constructor() { 
+    this.currentId = this.list.map( el => el.id )
+                               .reduce( (max, cur) => Math.max( max, cur ), 0 ) + 1;
+  }
+
+  getNotes = () =>{
+    return this.list;
+  }
+
+  getNote = (id) => {
+    return this.list.find( x => x.id === id);
+  }
+
+  addNote = () => {
+    const date = new Date().toJSON();
+    const newNote: Note = {id: this.currentId++, title: 'untitled', content: '...', category: 1, created: date};
+    this.list.push(newNote);
+    return newNote;
+  }
+
+  removeNote = (note:Note) => {
+    const i = this.list.indexOf(note);
+    if (i !== -1) {
+        this.list.splice(i, 1);
+    }
+    return this.list;
+  }
 
 }
